8e7a18040163ea7f20ec25c0ca3e6098ca752ac8
fxop0218 <francescoli0218@gmail.com>
2022-12-13 18:28:07 +0100

    Filter works, and show stats

--- main.py
+++ main.py
@@ -2,25 +2,62 @@ import pandas as pd
 import plotly.express as px
 import streamlit as srl
 
+CITY = "City"
+CUSTOMER_TYPE = "Customer_type"
+GENDER = "Gender"
+
 srl.set_page_config(page_title="Sales dashborad",
                     page_icon=":bar_chart:",
                     layout="wide")
 
+# noinspection PyTypeChecker
 df_mkt = pd.read_excel(
-    io="supermarkt.xlsx",
     engine="openpyxl",
+    io="supermarkt.xlsx",
     sheet_name="Sales",
-    skiprows=3,
     usecols="B:R",
     nrows=1000,
+    skiprows=3,
 )
-
-srl.dataframe(df_mkt)
-
 # Filter in the sidebar
 
 srl.sidebar.header("Filter")
-city = srl.sidebar.multiselect("Select the city:",
-                               options=df_mkt["City"].unique(),
-                               default=df_mkt["City"].unique()
+city = srl.sidebar.multiselect(f"Select the {CITY}",
+                               options=df_mkt[CITY].unique(),
+                               default=df_mkt[CITY].unique()
                                )
+gender = srl.sidebar.multiselect(f"Select the {GENDER}:",
+                                 options=df_mkt[GENDER].unique(),
+                                 default=df_mkt[GENDER].unique()
+                                 )
+custom_type = srl.sidebar.multiselect(f"Select the {CUSTOMER_TYPE}:",
+                                      options=df_mkt[CUSTOMER_TYPE].unique(),
+                                      default=df_mkt[CUSTOMER_TYPE].unique()
+                                      )
+
+df_selection = df_mkt.query(
+    f"City == @city & Customer_type == @custom_type & Gender == @gender"
+)
+
+srl.dataframe(df_selection)
+
+srl.title(":bar_chart: Sales Dashboard")
+srl.markdown("##")
+
+total_sales = int(df_selection["Total"].sum())
+avg_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(avg_rating, 0))
+avg_sale_x_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = srl.columns(3)
+with left_column:
+    srl.subheader("Total sales")
+    srl.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    srl.subheader("Avarage rating")
+    srl.subheader(f"{avg_rating} / {star_rating}")
+with right_column:
+    srl.subheader("Avarage sale x transaction")
+    srl.subheader(f"US $ {avg_sale_x_transaction}")
+
+srl.markdown("---")
